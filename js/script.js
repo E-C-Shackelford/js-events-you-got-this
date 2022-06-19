7bc4398259d2ba8ec5e849ecc69fccab6fa4cb9c
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-18 19:21:04 -0700

    Add a keydown event that closes the modal when the Esc key is pressed and refactor code to create two new function expressions that can be called in event handlers instead of duplicating orginal lines of code — one that closes the modal, and a second that opens the modal

--- js/script.js
+++ js/script.js
@@ -2,12 +2,33 @@ var button = document.querySelector("button");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
-button.addEventListener("click", function () {
+// create a function to open the modal
+var openModal = function () {
   modal.classList.add("show-modal");
   button.innerText = "You've got this!!!";
+};
+
+button.addEventListener("click", function () {
+  openModal();
 });
 
-modalX.addEventListener("click", function () {
+// since we had the same code in the function body of both the modalX and document event listeners, refactor the code into a single function expression
+var closeModal = function () {
   modal.classList.remove("show-modal");
   button.innerText = "Who's got this?";
+};
+
+modalX.addEventListener("click", function () {
+  closeModal();
+});
+
+// listen for a keydown event and pass the event to the callback function
+document.addEventListener("keydown", function (e) {
+  //console.log(e.key);
+  //check for a pressed Esc key and an open modal
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+  }
 });
